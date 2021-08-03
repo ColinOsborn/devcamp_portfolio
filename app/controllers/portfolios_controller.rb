17733fdaeedd257aad892844cbb68b9ddd719bce
colin <cosborn@patientslikeme.com>
2021-08-02 19:58:18 -0600

    more updates for creating portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,27 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.create!(portfolio_params)
+    if @portfolio.save
+      format.html { redirect_to portfolios_path, notice: 'Your portfolio item is live!' }
+    else
+      format.html { render :new }
+    end
+  end
+
+  private
+
+  def portfolio_select
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 end
