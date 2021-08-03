ee64626647d6d6766e86f6fd55a06624a5bbad4f
colin <cosborn@patientslikeme.com>
2021-08-02 22:06:09 -0600

    implemented friendly routes for blogposts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -56,12 +56,12 @@ class BlogsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.find(params[:id])
-    end
+  def set_blog
+    @blog = Blog.friendly.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def blog_params
-      params.require(:blog).permit(:title, :body)
-    end
+  # Only allow a list of trusted parameters through.
+  def blog_params
+    params.require(:blog).permit(:title, :body)
+  end
 end
