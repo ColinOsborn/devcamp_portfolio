b19553992256c33286e0ddf320569977fe42e4f9
colin <cosborn@patientslikeme.com>
2021-08-02 18:36:30 -0600

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget lectus pharetra justo laoreet suscipit in id justo. Suspendisse volutpat efficitur est, et eleifend risus convallis non. Donec mattis lacus ac bibendum tempus. Vivamus sagittis felis ligula, quis semper ex facilisis vitae. Nunc sapien dolor, ullamcorper eget posuere sed, commodo in massa. Donec interdum erat ac dui congue aliquam. Integer eu tempor purus. Sed erat ex, scelerisque id justo non, dictum mattis sem. Quisque sagittis, leo facilisis laoreet vehicula, purus ante ornare erat, quis malesuada nisl augue sit amet magna. Phasellus eget interdum lorem. Nullam ipsum dolor, malesuada id congue ac, viverra elementum ante. Morbi mollis auctor facilisis."
+  )
+end
+
+puts '10 blog posts created!'
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: rand(1..100)
+  )
+end
+
+puts '5 skills created!'
+
+9.times do |portfolio|
+  Portfolio.create!(
+    title: "Portfolio #{portfolio}",
+    subtitle: 'Great work done here',
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget lectus pharetra justo laoreet suscipit in id justo. Suspendisse volutpat efficitur est, et eleifend risus convallis non. Donec mattis lacus ac bibendum tempus. Vivamus sagittis felis ligula, quis semper ex facilisis vitae. Nunc sapien dolor, ullamcorper eget posuere sed, commodo in massa.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200",
+  )
+end
+
+puts '9 Portfolios created!'
