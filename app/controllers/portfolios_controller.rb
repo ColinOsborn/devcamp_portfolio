508b608561c4f01c73e48ff15db1ceb3e6a56a2b
colin <cosborn@patientslikeme.com>
2021-08-02 20:20:34 -0600

    added new and edit items to index page

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -17,6 +17,23 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def show
+    portfolio_select
+  end
+
+  def edit
+    portfolio_select
+  end
+
+  def update
+    portfolio = portfolio_select
+    if portfolio.update!(portfolio_params)
+      format.html { redirect_to portfolios_path, notice: 'The record successfully updated!' }
+    else
+      format.html { redirect_to :edit }
+    end
+  end
+
   private
 
   def portfolio_select
@@ -24,6 +41,6 @@ class PortfoliosController < ApplicationController
   end
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body)
+    params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image)
   end
 end
