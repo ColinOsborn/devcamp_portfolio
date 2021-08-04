a51c36bec821dfd3eb551998475da897de320949
colin <cosborn@patientslikeme.com>
2021-08-04 16:22:10 -0600

    handled defaults for images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,8 +2,14 @@ class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+  after_initialize :set_defaults
 
   def self.react
     where(subtitle: 'React')
+  end
+
+  def set_defaults
+    self.main_image ||= 'http://via.placeholder.com/600x400'
+    self.thumb_image ||= 'http://via.placeholder.com/350x200'
   end
 end
