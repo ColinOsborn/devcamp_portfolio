ef2910e7cfa8e03dcc357b4c05244ce96f94c834
colin <cosborn@patientslikeme.com>
2021-08-04 15:29:33 -0600

    validations for blogs, skils and portfolio attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -10,8 +10,8 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio = Portfolio.create!(portfolio_params)
-    if @portfolio.save
+    @portfolio = Portfolio.new(portfolio_params)
+    if @portfolio.save!
       format.html { redirect_to portfolios_path, notice: 'Your portfolio item is live!' }
     else
       format.html { render :new }
