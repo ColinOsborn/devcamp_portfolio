dda2d36a425888cb739fa0d166f737e9c435a950
colin <cosborn@patientslikeme.com>
2021-08-17 15:19:02 -0600

    added new parameters for handling name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_santizer.permit(:sign_up, keys: [:name])
+    devise_parameter_santizer.permit(:account_update, keys: [:name])
+  end
 end
