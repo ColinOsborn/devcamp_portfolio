be8dc39062f525df23776083b0a6a1ee9eeb6067
colin <cosborn@patientslikeme.com>
2021-11-30 13:03:07 -0700

    implemented aws integration for image uploads

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,16 +1,11 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
 
   storage :aws
-  # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Add an allowlist of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_allowlist
     %w(jpg jpeg gif png)
   end
